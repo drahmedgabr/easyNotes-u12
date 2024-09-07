2e599a6bdf8c52366450757b5494f3fcabe94f28
drahmedgabr <ahmadal4ever@gmail.com>
2024-09-07 15:26:37 +0300

    save notes

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const noteInput = document.getElementById("noteInput");
+const notesDiv = document.getElementById("notesDiv");
 
 var notesArray = [];
 
@@ -14,5 +15,44 @@ function addNote() {
     const newNote = noteInput.value;
     notesArray.push(newNote);
     noteInput.value = "";
-    console.log(notesArray);
+    saveNotes();
 }
+
+function saveNotes() {
+    const notesString = JSON.stringify(notesArray);
+    localStorage.setItem("notes", notesString);
+    getNotes();
+}
+
+function getNotes() {
+    const notesString = localStorage.getItem("notes");
+    if(notesString == null){
+        console.log("No notes found");
+    } else {
+        notesArray = JSON.parse(notesString);
+        showNotes();
+    }
+}
+
+function showNotes() {
+
+    notesDiv.innerHTML = "";
+
+    for (let index = 0; index < notesArray.length; index++) {
+        const element = notesArray[index];
+        
+        const newDiv = document.createElement("div");
+
+        const newP = document.createElement("p");
+        newP.innerText = element;
+        newDiv.appendChild(newP);
+
+        const newIcon = document.createElement("i");
+        newIcon.className = "bi bi-trash";
+        newDiv.appendChild(newIcon);
+
+        notesDiv.appendChild(newDiv);
+    }
+}
+
+getNotes();
