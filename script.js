8084057b84baf3274a5bc122d2f816c88c42d65a
drahmedgabr <ahmadal4ever@gmail.com>
2024-09-21 17:09:14 +0300

    show login

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 const noteInput = document.getElementById("noteInput");
 const notesDiv = document.getElementById("notesDiv");
 const zeroNotes = document.getElementById("zeroNotes");
+const loginDiv = document.getElementById("loginDiv");
+const optionsDiv = document.getElementById("optionsDiv");
+
 
 
 var notesArray = [];
+
+function showOptions() {
+    optionsDiv.style.display = "flex";
+    notesDiv.style.display = "none";
+    zeroNotes.style.display = "none";
+}
+
+function showHome() {
+    optionsDiv.style.display = "none";
+    loginDiv.style.display = "none";
+    notesDiv.style.display = "block";
+    getNotes();
+}
+
+function showLogin() {
+    optionsDiv.style.display = "none";
+    loginDiv.style.display = "flex";
+}
 
 function checkInput() {
     if(noteInput.value == ""){
